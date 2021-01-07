7f2e5ec6463af1aa0bfeb94881a5b0288e7aaa77
le.nguyenquocdai <le.nguyenquocdai@elinext.com>
2021-01-07 14:51:17 +0700

    add ramdom color and fix node-sass version

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import ColorBox from "./components/ColorBox";
 
 function App() {
   return (
     <div className="app">
       <h1>Welcome Reactjs</h1>
+      <ColorBox />
     </div>
   );
 }
